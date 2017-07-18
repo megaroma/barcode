caa36d05098e928e7597fbfca7f73259d14edd94
megaspy <darkromanovich@gmail.com>
2017-07-18 19:06:26 +1200

    signal handler

--- barcode.c
+++ barcode.c
@@ -262,6 +262,7 @@ void send_http(const char *bar_code) {
 
 int main(int argc, char* argv[]) {
     signal (SIGTERM, handler);
+    signal (SIGINT, handler);
 	char bar_code[200];
 	clearScreen();
 	printf("Starting\n");
